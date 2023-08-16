88173ae7133a003e6cbb58a9bac6182a0c9d1d90
demola234 <kolawoleoluwasegun567@gmail.com>
2023-08-16 14:57:33 +0100

    changing port

--- api/server.go
+++ api/server.go
@@ -35,7 +35,7 @@ func NewServer(config utils.Config, store db.Store) (*Server, error) {
 
 func (server *Server) setUpRouter() {
 	router := gin.Default()
-	gin.SetMode(gin.ReleaseMode)
+	router.SetTrustedProxies(nil)
 	router.POST("/user", server.createUser)
 	router.POST("/user/login", server.loginUser)
 	router.POST("/user/verify", server.verifyUser)
