16744816a916d2cb98879517cbe7dc8bc822901d
demola234 <kolawoleoluwasegun567@gmail.com>
2025-02-26 15:47:22 +0100

    chore: remove unused files and clean up codebase

--- db/migration/000001_schema.down.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS wallet;
-
-DROP TABLE IF EXISTS campaigns;
-
-DROP TABLE IF EXISTS donations;
-
-DROP TABLE IF EXISTS user_session;
-
-DROP TABLE if exists users cascade;
--- db/migration/000002_create_user_wallet_addresses_table.down.sql
+++
@@ -1,5 +0,0 @@
--- Drop the wallets table
-DROP TABLE IF EXISTS user_wallet_addresses;
-
--- Drop ENUM type
-DROP TYPE IF EXISTS user_wallet_addresses_statuses;
