0527504404e56b32f72c735af34618fc11ebdcdf
demola234 <kolawoleoluwasegun567@gmail.com>
2025-02-26 13:32:33 +0100

    fix: remove deprecated files and update .gitignore

--- db/query/campaign.sql
+++
@@ -1,9 +0,0 @@
--- name: CreateCampaignType :one
-
-INSERT INTO
-    campaigns (campaign_name, image)
-VALUES ($1, $2) RETURNING *;
-
--- name: GetAllCampaignType :many
-
-SELECT * FROM campaigns;
--- db/query/donations.sql
+++
@@ -1,8 +0,0 @@
--- name: GetAllActiveDonations :many
-
-SELECT *
-FROM donations
-WHERE deadline > now()
-ORDER BY created_at DESC
-LIMIT 10
-OFFSET 0;
