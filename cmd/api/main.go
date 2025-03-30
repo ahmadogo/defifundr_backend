1b32e78dae106f4c0b0820b8e505adedaa023a64
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-30 11:47:26 +0100

    feat: update database connection string to use configuration variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -55,7 +55,7 @@ func main() {
 	defer cancel()
 
 	// Connect to the database using the pgx driver with database/sql
-	conn, err := pgxpool.New(ctx, "postgres://postgres:postgres@localhost:5433/defifundr?sslmode=disable")
+	conn, err := pgxpool.New(ctx, configs.DBSource)
 	if err != nil {
 		logger.Fatal("Unable to connect to database", err, map[string]interface{}{
 			"db_source": configs.DBSource,
